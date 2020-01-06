7aca665779d7a5bdbb6918e55d21fdcfd4f0e85c
HaoyuHou <49450807+HaoyuHou@users.noreply.github.com>
2020-01-05 16:30:54 -0800

    Add files via upload

---
+++ linear_regression.py
@@ -0,0 +1,145 @@
+"""
+Do not change the input and output format.
+If our script cannot run your code or the format is improper, your code will not be graded.
+
+The only functions you need to implement in this template is linear_regression_noreg, linear_regression_invertible，regularized_linear_regression,
+tune_lambda, test_error and mapping_data.
+"""
+
+import numpy as np
+import pandas as pd
+
+###### Q1.1 ######
+def mean_absolute_error(w, X, y):
+    """
+    Compute the mean absolute error on test set given X, y, and model parameter w.
+    Inputs:
+    - X: A numpy array of shape (num_samples, D) containing test feature.
+    - y: A numpy array of shape (num_samples, ) containing test label
+    - w: a numpy array of shape (D, )
+    Returns:
+    - err: the mean absolute error
+    """
+    #####################################################
+    # TODO 1: Fill in your code here #
+    #####################################################
+    err=None
+    forecast=X.dot(w)
+    err=(abs(forecast-y)).mean()
+    return err
+###### Q1.2 ######
+def linear_regression_noreg(X, y):
+  """
+  Compute the weight parameter given X and y.
+  Inputs:
+  - X: A numpy array of shape (num_samples, D) containing feature.
+  - y: A numpy array of shape (num_samples, ) containing label
+  Returns:
+  - w: a numpy array of shape (D, )
+  """
+  #####################################################
+  #	TODO 2: Fill in your code here #
+  #####################################################		
+  w=None
+  a=np.linalg.inv(X.T.dot(X))
+  w=a.dot(X.T).dot(y)
+  return w
+
+###### Q1.3 ######
+def linear_regression_invertible(X, y):
+    """
+    Compute the weight parameter given X and y.
+    Inputs:
+    - X: A numpy array of shape (num_samples, D) containing feature.
+    - y: A numpy array of shape (num_samples, ) containing label
+    Returns:
+    - w: a numpy array of shape (D, )
+    """
+    #####################################################
+    # TODO 3: Fill in your code here #
+    #####################################################
+    w=None
+    inner=X.T.dot(X)
+    I=0.1*np.identity(inner.shape[0])
+    l,v=np.linalg.eig(inner)
+    while np.abs(l).min()<0.00001:
+        inner+=I
+        l,v=np.linalg.eig(inner)
+    w=np.linalg.inv(inner).dot(X.T).dot(y)
+    return w   
+
+
+###### Q1.4 ######
+def regularized_linear_regression(X, y, lambd):
+    """
+    Compute the weight parameter given X, y and lambda.
+    Inputs:
+    - X: A numpy array of shape (num_samples, D) containing feature.
+    - y: A numpy array of shape (num_samples, ) containing label
+    - lambd: a float number containing regularization strength
+    Returns:
+    - w: a numpy array of shape (D, )
+    """
+  #####################################################
+  # TODO 4: Fill in your code here #
+  #####################################################		
+    w=None
+    inner=X.T.dot(X)
+    M=np.identity(inner.shape[0])
+    M*=lambd
+    inner+=M
+    a=np.linalg.inv(inner)
+    w=a.dot(X.T).dot(y)
+    return w
+
+###### Q1.5 ######
+def tune_lambda(Xtrain, ytrain, Xval, yval):
+    """
+    Find the best lambda value.
+    Inputs:
+    - Xtrain: A numpy array of shape (num_training_samples, D) containing training feature.
+    - ytrain: A numpy array of shape (num_training_samples, ) containing training label
+    - Xval: A numpy array of shape (num_val_samples, D) containing validation feature.
+    - yval: A numpy array of shape (num_val_samples, ) containing validation label
+    Returns:
+    - bestlambda: the best lambda you find in lambds
+    """
+    #####################################################
+    # TODO 5: Fill in your code here #
+    #####################################################		
+    best_lambda=None
+    max_mae=999999.9
+    i=-19
+    while i<20:
+        b=10**i
+        w=regularized_linear_regression(Xtrain,ytrain,b)
+        mae=mean_absolute_error(w,Xval,yval)
+        if mae<max_mae:
+            best_lambda=b
+            max_mae=mae
+        i+=1
+    return best_lambda
+    
+
+###### Q1.6 ######
+def mapping_data(X, power):
+    """
+    Mapping the data.
+    Inputs:
+    - X: A numpy array of shape (num_training_samples, D) containing training feature.
+    - power: A integer that indicate the power in polynomial regression
+    Returns:
+    - X: mapped_X, You can manully calculate the size of X based on the power and original size of X
+    """
+    #####################################################
+    # TODO 6: Fill in your code here #
+    #####################################################		
+    List=[X]
+    i=1
+    while i<power:
+        List.append(List[i-1]*X)
+        i+=1
+    result=np.concatenate(List, axis=1)
+    return result
+
+
